9f6852290eae8ef29713d77f1dbc4fd361838692
rishabhhh2005 <159003946+rishabhhh2005@users.noreply.github.com>
2024-05-20 08:26:10 +0530

    Created Display Menu Function.c

---
+++ Display Menu Function.c
@@ -0,0 +1,8 @@
+// Function to display the menu options
+void menu() {
+    printf("********** ATM Menu **********\n");
+    printf("1. Check Balance\n");
+    printf("2. Deposit Money\n");
+    printf("3. Withdraw Money\n");
+    printf("4. Exit\n");
+}
