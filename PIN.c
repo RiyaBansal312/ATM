005d05eeecdc73bd228bd1e4f4ee2b4afd5b6753
rishabhhh2005 <159003946+rishabhhh2005@users.noreply.github.com>
2024-05-20 08:28:50 +0530

    Create PIN.c

---
+++ PIN.c
@@ -0,0 +1,17 @@
+// PIN verification process
+    while (attempts < maxAttempts && !isVerified) {
+        printf("Enter your PIN: ");
+        scanf("%d", &enteredPIN);
+        if (verifyPIN(enteredPIN, actualPIN)) {
+            isVerified = true;
+            printf("PIN verified successfully!\n");
+        } else {
+            attempts++;
+            printf("Incorrect PIN. You have %d attempt(s) left.\n", maxAttempts - attempts);
+        }
+    }
+    
+    if (!isVerified) {
+        printf("Maximum attempts reached. Exiting...\n");
+        exit(0);
+    }
